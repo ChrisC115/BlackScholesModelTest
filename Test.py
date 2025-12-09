61aad211a50294535473f7967f05dbafb28fc1a7
ChrisC115 <notebooklm2000@gmail.com>
2025-12-09 17:22:53 +0000

    first

--- Test.py
+++ Test.py
@@ -1,10 +1,24 @@
-import pandas
+#pip install pandas numpy yfinance matplotlib
+
+import pandas as pd
 import numpy
-import matplotlib
+import matplotlib.pyplot as plt
 import yfinance as yf
 
-apple = yf.Ticker("APPL")
 
+ticker_strings = ['AAPL', 'MSFT', 'GOOG']
+df_list = []
 
-apple = apple.history(period = "1y")
-print(apple)
+for ticker_symbol in ticker_strings:
+    # Download data
+    stock_data = yf.download(ticker_symbol, period='1mo')
+    # Add a 'ticker' column for identification
+    stock_data['Ticker'] = ticker_symbol
+    # Append the DataFrame to the list
+    df_list.append(stock_data)
+
+# 'df_list' is now a list containing three separate pandas DataFrames
+print("\nNumber of DataFrames in the list:", len(df_list))
+print("Head of the first DataFrame (AAPL):")
+print(df_list[0].head())
+
